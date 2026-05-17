385b2ea76651c99686b14d905f7c79b7e3519adf
kdziedzicKWS <kdziedzic@keywordsstudios.com>
2026-05-18 00:57:26 +0200

    Fix requests import and API call for watermelon
    Re-import requests and fetch watermelon data from API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
 # Import python packages
 import streamlit as st
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -45,4 +42,6 @@ if ingredient_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order +'!', icon="✅")
       
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
